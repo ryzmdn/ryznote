c466556466163c829ac7feea4fd7d8708cb1567b
R <rizkyramadhanpd@gmail.com>
2025-06-27 11:56:12 +0700

    refactor: GoogleTranslate default export

--- src/components/GoogleTranslate.tsx
+++ src/components/GoogleTranslate.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-export default function GoogleTranslate() {
+export function GoogleTranslate() {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
