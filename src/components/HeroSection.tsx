0dbc58916054283348d1936a71d4f0725a9f7f0f
R <rizkyramadhanpd@gmail.com>
2025-07-25 09:29:44 +0700

    feat: adding a new variant blog card in the hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,30 +1,80 @@
 import { Button } from "@/components/common/Button";
+import { UniqueCard } from "@/components/blog/UniqueCard";
+import { Post } from "@/types/wordpress";
 
-export function HeroSection() {
+async function getPosts(): Promise<Post[]> {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_WORDPRESS_API}/posts?_embed`,
+      {
+        next: { revalidate: 60 },
+      }
+    );
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
+}
+
+export async function HeroSection() {
+  const posts = await getPosts();
+
   return (
-    <div className="mx-auto max-w-4xl py-20">
-      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-        <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-          Check out my wordpress profile too.{" "}
-          <Button variant="default" href="https://ryzmdn.wordpress.com/" className="font-medium text-indigo-600">
-            <span aria-hidden="true" className="absolute inset-0" /> Visit Now <span aria-hidden="true">&rarr;</span>
-          </Button>
+    <>
+      <div className="mx-auto max-w-4xl py-20">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            Check out my wordpress profile too.{" "}
+            <Button
+              variant="default"
+              href="https://ryzmdn.wordpress.com/"
+              className="font-medium text-indigo-600"
+            >
+              <span aria-hidden="true" className="absolute inset-0" /> Visit Now{" "}
+              <span aria-hidden="true">&rarr;</span>
+            </Button>
+          </div>
+        </div>
+        <div className="text-center">
+          <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+            Welcome to My Personal Blog
+          </h1>
+          <p className="mt-8 text-base/7 text-pretty text-gray-500 sm:text-lg/8">
+            Discover stories, insights, and inspiration from my journey. Explore
+            articles on technology, creativity, and everyday life—written to
+            spark ideas and connect with fellow readers.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Button
+              href="/blog/category/all?page=1"
+              className="px-5 py-3 rounded-xl"
+            >
+              View All Blogs
+            </Button>
+            <Button
+              variant="secondary"
+              href="/collections"
+              className="px-5 py-3 rounded-xl"
+            >
+              See All Collections
+            </Button>
+          </div>
         </div>
       </div>
-      <div className="text-center">
-        <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-          Welcome to My Personal Blog
-        </h1>
-        <p className="mt-8 text-base/7 text-pretty text-gray-500 sm:text-lg/8">Discover stories, insights, and inspiration from my journey. Explore articles on technology, creativity, and everyday life—written to spark ideas and connect with fellow readers.</p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Button href="/blog/category/all?page=1" className="px-5 py-3 rounded-xl">
-            View All Blogs
-          </Button>
-          <Button variant="secondary" href="/collections" className="px-5 py-3 rounded-xl">
-            See All Collections
-          </Button>
-        </div>
+
+      <div className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
+        {posts.slice(0, 3).map((post) => (
+          <UniqueCard
+            key={post.id}
+            contentHtml={post.content.rendered}
+            title={post.title.rendered}
+            url={post.slug}
+            category={post._embedded?.["wp:term"]?.[0]?.[0].name}
+            datetime={post.date}
+          />
+        ))}
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/blog/UniqueCard.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Time } from "../common/Time";
+import { formatDate } from "@/utils/fotmatDate";
+import picture from "@/assets/picture.webp";
+import { Button } from "../common/Button";
+import { convertEncode } from "@/utils/encode";
+
+interface Props {
+  contentHtml: string;
+  datetime: string;
+  url: string;
+  category?: string;
+  title: string;
+}
+
+export function UniqueCard({
+  contentHtml,
+  datetime,
+  url,
+  category,
+  title,
+}: Readonly<Props>) {
+  const [thumbnail, setThumbnail] = useState<string | null>(null);
+
+  useEffect(() => {
+    const doc = new DOMParser().parseFromString(contentHtml, "text/html");
+    const img = doc.querySelector("img[data-orig-file]");
+    const imgSrc =
+      img?.getAttribute("data-orig-file") || "https://placehold.co/600x400";
+    setThumbnail(imgSrc);
+  }, [contentHtml]);
+
+  return (
+    <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+      {thumbnail ? (
+        <Image
+          fill
+          src={thumbnail}
+          alt={`blog-thumbnail-${title.replace(/ /gi, "-").toLowerCase()}`}
+          className="absolute inset-0 -z-10 size-full object-cover"
+        />
+      ) : (
+        <div>h</div>
+      )}
+      <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+      <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+
+      <div className="flex flex-wrap items-center gap-x-4 gap-y-1 overflow-hidden text-sm/6 text-gray-300">
+        <Time
+          date={new Date(datetime).toLocaleDateString()}
+          className="text-gray-300"
+        >
+          {formatDate(new Date(datetime).toLocaleDateString())}
+        </Time>
+        <div className="flex items-center gap-x-4">
+          <svg
+            viewBox="0 0 2 2"
+            className="size-0.5 flex-none fill-gray-50/50"
+          >
+            <circle r={1} cx={1} cy={1} />
+          </svg>
+          <div className="flex gap-x-2.5">
+            <Image
+              src={picture}
+              alt="me"
+              className="size-6 flex-none rounded-full bg-gray-50/10"
+            />
+            Ryzmdn
+          </div>
+        </div>
+      </div>
+      <h3 className="mt-3 text-xl/7 font-semibold text-gray-100">
+        <Button variant="default" href={`/blog/read/${url}`} className="line-clamp-2">
+          <span className="absolute inset-0" />
+          {convertEncode(title)}
+        </Button>
+      </h3>
+    </article>
+  );
+}
