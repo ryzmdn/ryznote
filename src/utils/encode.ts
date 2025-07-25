78bbd3000cd89102bf8a2f4c775e604f52643442
R <rizkyramadhanpd@gmail.com>
2025-07-25 11:25:04 +0700

    feat: HTML symbol conversion adjustment

--- src/utils/encode.ts
+++ src/utils/encode.ts
@@ -6,7 +6,9 @@ export function convertEncode(text: string): string {
   let convertedText = text;
 
   convertedText = convertedText.replace(/&nbsp;|\u00A0/g, " ");
-  convertedText = convertedText.replace(/&#8217;/g, "'");
+  convertedText = convertedText.replace(/&#8217;/g, "’");
+  convertedText = convertedText.replace(/&#8220;/g, "“");
+  convertedText = convertedText.replace(/&#8221;/g, "”");
 
   return convertedText;
 }
