842f06e0da6cbf6e8bb5369551b32ca068516474
R <rizkyramadhanpd@gmail.com>
2025-08-14 10:06:27 +0700

    feat: layout updates in the footer section

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,30 +1,16 @@
 import { ReactNode } from "react";
-import axios from "axios";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { ThemeProvider } from "@/context/ThemeProvider";
-import { Category } from "@/types/wordpress";
-
-async function getPosts() {
-  try {
-    const response = await axios.get<Category[]>(`${process.env.NEXT_PUBLIC_WORDPRESS_API}/categories`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    return [];
-  }
-}
 
 export async function Provider({ children }: Readonly<{ children: ReactNode }>) {
-  const categories = await getPosts();
-  
   return (
     <ThemeProvider>
       <Header />
       <main className="mx-auto max-w-6xl w-full px-6 lg:px-8">
         {children}
       </main>
-      <Footer categories={categories} />
+      <Footer />
     </ThemeProvider>
   );
 }
